eed078e52aa85f0e54b8b0d2141459358826bb9b
Elcin Mammadov <elcinelcin57@gmail.com>
2024-07-06 13:33:08 +0400

    Tailwindcss ile olusturulan site denemesi

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+
+      spacing: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
